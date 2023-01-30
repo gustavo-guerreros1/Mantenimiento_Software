d47e3d026b3314829feeed6d9737b9fa3a343af6
DiegoByte722 <diego.carrion@unmsm.edu.pe>
2023-01-30 16:07:39 -0500

    Implementacion de autocompletado en el panel de busqueda

--- src/main/java/com/springboot/panecillos/app/service/ProductoServiceImpl.java
+++ src/main/java/com/springboot/panecillos/app/service/ProductoServiceImpl.java
@@ -13,6 +13,14 @@ import com.springboot.panecillos.app.models.domain.Producto;
 
 @Service
 public class ProductoServiceImpl implements IProductoService{
+	
+
+    // variables
+    Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    Producto pro;
+    String sql;
 
 	@Autowired 
 	private IProductoDao productodao;
@@ -21,7 +29,36 @@ public class ProductoServiceImpl implements IProductoService{
 	public List<Producto> findAll() {
 		return (List<Producto>) productodao.findAll();
 	}
-
+	
+    public Producto findByName(String t) throws Exception {
+        pro = null;
+        try {
+            cn = AccesoDB.getConnection();
+            ps = cn.prepareStatement("select * from productos where descripcion=?");
+            //preparar valor del parametro
+            ps.setString(1, t);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                pro = new ProductoTO();
+                //asignar valores al objeto pro
+                pro.setIdProducto(rs.getString(1));
+                pro.setDescripcion(rs.getString(2));
+                pro.setIdLinea(rs.getInt(3));
+                pro.setIdProveedor(rs.getString(4));
+                pro.setPrecioCompra(rs.getDouble(5));
+                pro.setPrecioVenta(rs.getDouble(6));
+                pro.setStock(rs.getInt(7));
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            cn.close();
+        }
+        return pro;
+    }
+    
 	@Override
 	public Producto buscarPorId(Long id) {
 		
