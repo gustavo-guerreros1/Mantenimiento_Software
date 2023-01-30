723968097b55e9712ab5b572ff64cdd668219faf
LDChristian <77200735+LDChristian@users.noreply.github.com>
2023-01-30 15:00:35 -0500

    Modificando los css

---
+++ bin/src/main/resources/static/js/chart-area-demo.js
@@ -0,0 +1,54 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["11 Abril", "12 Abril", "13 Abril", "14 Abril", "15 Abril", "16 Abril", "17 Abril", "18 Abril", "19 Abril", "20 Abril", "21 Abril", "22 Abril", "23 Abril"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.5,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [300, 350, 300, 350, 100, 350, 150, 300, 150, 100, 300, 300, 400],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 10
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 5,
+          max: 400,
+          maxTicksLimit: 30
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
